152279b982bdfff692355e181962b51cf69e33d7
Julien Savoie <julien@savoie.dev>
2020-03-03 16:42:06 -0500

    Debut custom category

---
+++ category-evenement.php
@@ -0,0 +1,27 @@
+<?php
+/**
+* Template pour la categories événement
+*/
+ 
+get_header(); ?> 
+
+<main class="evenement-main">
+    <h1>Nos événements importants cette année</h1>
+    <div class="evenement-grille">
+        <div class="mois" id="septembre">
+            <div>test1</div>
+            <div>test2</div>
+        </div>
+        <div class="mois" id="octobre">
+        <div>test3</div>
+        </div>
+        <div class="mois" id="novembre">
+        <div>test4</div>
+        <div>test5</div>
+        <div>test6</div>
+        </div>
+        
+    </div>
+</main>
+
+<?php get_footer(); ?>
