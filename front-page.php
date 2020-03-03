04eecdb9348a2eb74a5020b7a4c158926116a6df
Julien Savoie <julien@savoie.dev>
2020-03-03 15:31:05 -0500

    Header Initial

---
+++ front-page.php
@@ -0,0 +1,70 @@
+<?php
+/**
+ * The template for displaying the front-page
+ *
+ * This is the template that displays the front-page by default.
+ *
+ * @link https://developer.wordpress.org/themes/basics/template-hierarchy/
+ *
+ * @package pied-cormorant
+ */
+
+get_header();
+?>
+	<h1 class="pc-site-title"><?php bloginfo( 'name' ); ?></h1>
+	<div id="primary" class="content-area">
+		<main id="main" class="site-main">
+
+		<?php 
+		// the query
+		$the_query = new WP_Query( array(
+			'category_name' => 'nouvelle',
+			'posts_per_page' => 3,
+		)); 
+		?>
+
+		<?php if ( $the_query->have_posts() ) : ?>
+		<?php while ( $the_query->have_posts() ) : $the_query->the_post(); ?>
+
+		<a href="<?php the_permalink(); ?>"><h1><?php the_title(); ?></h1></a>
+		<p><?php the_excerpt(); ?></p>
+
+		<?php endwhile; ?>
+		<?php wp_reset_postdata(); ?>
+
+		<?php else : ?>
+		<span><?php __('Pas de nouvelle'); ?></span>
+		<?php endif; ?>
+        
+		</main><!-- #main -->
+	</div><!-- #primary -->
+	<div class='section-evenements'>
+		<div class="section-evenements-conteneur">
+			<h1>Événements!</h1>
+			<ul>
+				<?php 
+				// the query
+				$the_query = new WP_Query( array(
+					'category_name' => 'evenement',
+					'posts_per_page' => 4,
+				)); 
+				?>
+
+				<?php if ( $the_query->have_posts() ) : ?>
+				<?php while ( $the_query->have_posts() ) : $the_query->the_post(); ?>
+				
+				<li><div><?php the_post_thumbnail() ?><h3><?php the_title(); ?></h3></div></li>
+
+				<?php endwhile; ?>
+				<?php wp_reset_postdata(); ?>
+
+				<?php else : ?>
+				<li><?php __("Pas d'événement"); ?></li>
+				<?php endif; ?>
+			</ul>
+		</div>
+	</div>
+
+<?php
+get_sidebar();
+get_footer();
---
+++ header.php
@@ -0,0 +1,52 @@
+<?php
+/**
+ * The header for our theme
+ *
+ * This is the template that displays all of the <head> section and everything up until <div id="content">
+ *
+ * @link https://developer.wordpress.org/themes/basics/template-files/#template-partials
+ *
+ * @package tp_SavoieJulien
+ */
+
+?>
+<!doctype html>
+<html <?php language_attributes(); ?>>
+<head>
+	<meta charset="<?php bloginfo( 'charset' ); ?>">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<link rel="profile" href="https://gmpg.org/xfn/11">
+
+	<?php wp_head(); ?>
+</head>
+
+<body <?php body_class(); ?>>
+<div id="page" class="site">
+	<header id="masthead" class="site-header">
+		<div class="site-branding">
+			<?php
+			the_custom_logo();
+			if ( is_front_page() && is_home() ) :
+				?>
+				<h1 class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></h1>
+				<?php
+			else :
+				?>
+				<p class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></p>
+				<?php
+			endif;
+			?>
+		</div><!-- .site-branding -->
+
+		<nav id="site-navigation" class="main-navigation">
+			<!-- <button class="menu-toggle" aria-controls="primary-menu" aria-expanded="false"><?php esc_html_e( 'Primary Menu', 'tp_js' ); ?></button> -->
+			<?php
+			wp_nav_menu( array(
+				'theme_location' => 'menu-1',
+				'menu_id'        => 'primary-menu',
+			) );
+			?>
+		</nav><!-- #site-navigation -->
+	</header><!-- #masthead -->
+	<img class="hero-img-pc" src="https://i.pinimg.com/originals/2e/4c/53/2e4c53158f4fb3efef58c7dc9a3fa7b9.jpg" alt="Pied Cormorant">
+	<div id="content" class="site-content">
